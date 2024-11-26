b535fe4e02a1726cdb5b0ac9d809a66fbe9595e1
gideonokoroafor <okoroaforgid@gmail.com>
2024-11-26 13:31:53 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gideon Okoroafor on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
