dd920dd5563dbf8152915c4232eae48277ce523e
gideonokoroafor <okoroaforgid@gmail.com>
2024-11-28 14:27:25 -0600

    implemented Expenseitem with a unique idetifier and sheets to add new expenses

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Gideon Okoroafor on 11/28/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
