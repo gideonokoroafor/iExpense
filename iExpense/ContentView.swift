37bee2837703ad8a452b2dfdbc6de10c2ef917c3
gideonokoroafor <okoroaforgid@gmail.com>
2024-11-28 14:54:23 -0600

    implemeted local storage

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -16,7 +16,23 @@ struct ExpenseItem: Identifiable {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init(){
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodeditems = try?  JSONDecoder().decode([ExpenseItem].self, from: savedItems){
+                items = decodeditems
+                return
+            }
+        }
+        items = []
+    }
 }
 
 struct ContentView: View {
@@ -27,7 +43,16 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack (alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                                .font(.subheadline)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
