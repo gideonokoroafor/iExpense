71e18bfc180da8e8a33abbdc116a4a78fdb73c86
gideonokoroafor <okoroaforgid@gmail.com>
2024-11-28 14:28:08 -0600

    implemented a container to add new expenses

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showAddExpense = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showAddExpense){
+                AddView(expenses: expenses)
+            }
         }
-        .padding()
+    }
+    
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
